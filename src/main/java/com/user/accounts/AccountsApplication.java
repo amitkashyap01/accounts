512cbde5b03a877d931955fa84c552ee88e1de21
Amit Kumar Kashyap <amit_kashyap01@infosys.com>
2018-06-06 23:10:26 +1000

    Implemented the resttemplate to cosume items data

--- src/main/java/com/user/accounts/AccountsApplication.java
+++ src/main/java/com/user/accounts/AccountsApplication.java
@@ -2,11 +2,23 @@ package com.user.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class AccountsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
+		//SpringApplication.run(AccountsApplication.class, args);
+		
+		String urlForItem = "http://localhost:8090/items/5";
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		ResponseEntity<String> entity = restTemplate.getForEntity(urlForItem, String.class);
+		
+		System.out.println("Output: "+entity.getBody());
+		
+		
 	}
 }
