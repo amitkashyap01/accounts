f8a58a80f2f12e6a22c531bddac0915f26695d45
Amit Kumar Kashyap <amit_kashyap01@infosys.com>
2018-06-11 20:06:30 +1000

    Implemented Ribbon for Load Balacing

--- src/main/java/com/user/accounts/AccountsApplication.java
+++ src/main/java/com/user/accounts/AccountsApplication.java
@@ -4,15 +4,11 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import com.user.accounts.controller.AccountController;
 
--- src/main/java/com/user/accounts/controller/AccountController.java
+++ src/main/java/com/user/accounts/controller/AccountController.java
@@ -3,9 +3,12 @@ package com.user.accounts.controller;
 import java.io.IOException;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -16,33 +19,42 @@ import org.springframework.web.client.RestTemplate;
 
 public class AccountController {
 	
-	@Autowired
-	private DiscoveryClient discoveryClient;
+	private static final Logger LOGGER = LoggerFactory.getLogger(AccountController.class);
 	
+	// To discover the producer application from Eureka Server
+//	@Autowired
+//	private DiscoveryClient discoveryClient;
+	
+	
+	//To load balacer discovery
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
 	
 	public void getItems() throws RestClientException, IOException{
 				
-			List<ServiceInstance> instances = discoveryClient.getInstances("inventoryapp");
+			// For Normal Discovery
+			//List<ServiceInstance> instances = discoveryClient.getInstances("inventoryapp");
+			//ServiceInstance serviceInstance = instances.get(0);
 		
-			ServiceInstance serviceInstance = instances.get(0);
-			
+			// For LoadBalacer Discovery
+			ServiceInstance serviceInstance = loadBalancerClient.choose("inventoryapp");
+		
 			String baseUrl = serviceInstance.getUri().toString();
 			
+			LOGGER.info("Discovered producer's URL: "+baseUrl);
 			
 			String urlForItem = baseUrl + "/items";
-			
 			RestTemplate restTemplate = new RestTemplate();
 			
 			ResponseEntity<String> entity = null;
 			try{
-				
-				
+				LOGGER.info("Calling GET on URL: "+urlForItem);
 				entity = restTemplate.getForEntity(urlForItem, String.class);
 			}catch(Exception ex){
 				System.out.println(ex.toString());
 			}
 			
-			System.out.println("Output: "+entity.getBody());
+			LOGGER.info("Received Value from GET "+urlForItem+": "+entity.getBody());
 	}
 	
 	public static HttpEntity<?> getHeaders() throws Exception{
